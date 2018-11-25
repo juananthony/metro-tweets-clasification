14012de8ab1407c3b75823f856c4137d40df944e
Juan Antonio <ruchin10@gmail.com>
2018-11-26 00:24:12 +0100

    added host port to an variable environment

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     server: {
-        port: 4000
+        port: process.env.SERVER_PORT
     },
     db: {
         //uri: 'mongodb://' + process.env.MONGO_USER + ':' + process.env.MONGO_PASS + '@' + 'Cluster0-shard-0/cluster0-shard-00-00-rxae4.mongodb.net:27017,cluster0-shard-00-01-rxae4.mongodb.net:27017,cluster0-shard-00-02-rxae4.mongodb.net:27017' + '/classification' + 'ssl=true&authSource=admin'
