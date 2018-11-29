097fd56892a47bb045a46d694b49edd8bcaec64e
Juan Antonio <ruchin10@gmail.com>
2018-11-29 13:40:57 +0100

    Mongoose model updated

--- models/tweet.js
+++ models/tweet.js
@@ -1,10 +1,20 @@
 import mongoose from '../db.js';
 import uuid from 'node-uuid';
+import UserSchema from './user';
 
 const TweetSchema = mongoose.Schema({
     id: Number,
     text: String,
-    classified: String
+    classified: String,
+    user: {
+        id: Number,
+        name: String,
+        url: String,
+        location: String,
+        description: String
+    },
+    created_at: Date,
+    timestamp_ms: Number
 });
 
 TweetSchema.set('collection', 'classify');
